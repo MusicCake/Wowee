c678ad04fbfeb029f411550074d33559d3e6c743
MusicCake <32333695+MusicCake@users.noreply.github.com>
2019-09-23 23:40:14 -0400

    Added MotivateMe.lua
    !motivateme now works as well

---
+++ Wowee/MotivateMe.lua
@@ -0,0 +1,175 @@
+-- TODO:
+--	Add in cross-client communication so only one client send message
+--	Make it trigger on a wipe
+
+
+-- message('Is this working?')
+GET_MOTIVATED = {
+"\'I love getting a huge one\' - Veradis", -- 1
+"\'What ego?\' - Twitch", -- 2
+"I don’t want Luda to yell at me", -- 3
+"\'It doesn’t even hit that hard. Oh that hits hard\' - Luda", -- 4
+"\'I prefer tanking with my butt\' - Veradis", -- 5
+"\'I would like for the guild to note, that I am thanking you for your actions, Dhaline\' - Jess", -- 6
+"\'I’m impressed I’m doing as well as I am, not knowing anything\' - Funkzorz", -- 7
+"\'I like Bendak cuz he’s quiet.\' - Samelf", -- 8
+"\'Anton, you’re assuming anyone else actually looked at the guides.\' - Bendak", -- 9
+"\'Ah, pillaging. The honest living.\' - Saibric", -- 10
+"\'Catch me Tomo!\' - Koehn", -- 11
+"\'You guys remember when demons were just demons and not aliens?\' - Saibric", -- 12
+"\'I wish I could be a zerg.\' - Tomo", -- 13
+"\'I told him Mountain Dew toothpaste was a bad idea.\' - Saibric", -- 14
+"\'Well I wasn’t listening then.\' - Tawa", -- 15
+"\'It’s probably fine.\' - Dhame", -- 16
+"\'Can we just call Tomo Cousin Slowhands now?\' - Twitch", -- 17
+"\'It worked out for him though, he became an asshole.\' - Logan", -- 18
+"(Gnome voice) \'Ah-ha! Nisth is dead!\' - Tomo", -- 19
+"\'You know, there’s a guy at work and I didn’t like him, it took me three weeks to figure out why. His name’s Anton.\' - Logan", -- 20
+"\'I fucking hate you people.\' - Jonidas", -- 21
+"\'Hey, can we go back to bear puns?\' - Samhunter", -- 22
+"\'I look forward to burning to death.\' - Tomo", -- 23
+"\'Come back magical clothing yack, I need you.\' - Tomo", -- 24
+"\'Good.\' - Samelf", -- 25
+"\'You don’t need a cloak. It’s not even cold in here.\' - Koehn", -- 26
+"\'DODGE DODGE DODGE!!!\' - Anton", -- 27
+"\'Make sure you’ve stopped heals on David.\' - Twitch", -- 28
+"\'I apparently don’t take enough drugs to participate in this medicine game.\' - Valores", -- 29
+"\'Just eat your food!\' - Koehn", -- 30
+"\'Why am I even talking to any of you?\' - Tomo", -- 31
+"\'Slowly absorbing smart particles.\' - Funkzorz", -- 32
+"\'Apparently he does some mechanics?\' - Twitch", -- 33
+"\'Wait, why did I deploy all my cooldowns, now I can’t...ah, I suck.\' - Tomo", -- 34
+"\'Look at me, I’m Tomocan, discipline user.\' - Tomo", -- 35
+"\'Anton thinks he’s funny.\' - Ray", -- 36
+"\'Hang on, I’ve gotta collect purple.\' - Saibric", -- 37
+"*open mic* - Ben", -- 38
+"*doorbell* - Ben's dog", -- 39
+"\'I believe in people.\' - Anton", -- 40
+"\'I’d rather not look like an extremely flamboyant warrior with a flair for transmog\' - Akronos", -- 41
+"\'You’d be surprised at what I don’t know\' - Prixah", -- 42
+"\'Come on guys this isnt hard...\' - Jake", -- 43
+"\'...\' - Electroburst", -- 44
+"\'Damnit Bakes!\'", -- 45
+"\'There's more armor on my butt\' - Veradis", -- 46
+"*sobbing* - Veradis", -- 47
+"\'Twitch is a crutch\'", -- 48
+"\'Gear is a crutch\' - Twitch", -- 49
+"\'I'm surprised you made it this long without knowing that.\' - Dhaline", -- 50
+"\'I was only gone for five minutes, how did this become the Hunger Games?\' - Dhaline", -- 51
+"\'Death is a Cooldown\' - Tomocan", -- 52
+"\'just fuck me\' - Loganisis", -- 53
+"\'Logan, I find it easier to just move taunt off your bars\' - Funkzorz", -- 54
+"\'I miss Karahd\' - Samelf", -- 55
+"\'If I didn't like the people I hate, I would have left this guild a long time ago.\' - Samelf", -- 56
+"\'I need to stop saying things.\' - Samelf", -- 57
+"\'Johndie, our savior!\' - Dhaline", -- 58
+"\'GET OUT OF THE CHEST! GET OUT OF THE CHEST! GET OUT OF THE CHEST!\' - Anton", -- 59
+"\'I'm just sad that kids can't find something more fun to do with condoms\' - Prixah", -- 60
+"\'There are many reasons to dab\' -Samelf", -- 61
+"*rustling noises from Ben's open mic* \'It's like I'm in his house\' - Jess", -- 62
+"\'You're up the stairs, I'm nature locked. Is there any way I could more unheal you?\' - Jess", -- 63
+"\'The cauldron will still be there after we wipe\' - Bendak", -- 64
+"\'Fight for my amusement.\' - Samelf", -- 65
+"\'It's ok, me and Saibric give it to each other.\' - Gier", -- 66
+"\'I understand dabbing now!\' - Ben", -- 67
+"\'The Motivate Me app is a circle jerk for the clique\' - Zilluzionist", -- 68
+"\'I feel like mines are the cut potatoes of our guild\' - Kragordan", -- 69
+"*dabs* - Samelf really did this, honest.", -- 70
+"\'I can't word, my apologies.\' - Prixah", -- 71
+"\'Loose clumps of Mountain Dew\' - Dhaline",  -- 72
+"\'Shut up, Anton.\' - Tomo", -- 73
+"\'Shut up, Anton.\' - Tomo (in his gnome voice)", -- 74
+"\'Can't wait until Anton screws this up with blink.\' - Magie", -- 75
+"\'I am full of confidence.\' - Dhaline", -- 76
+"\'I'm glad you watched the video I told everyone to watch.\' - Dhaline", -- 77
+"\'My flask has 8 minutes left so I'll need a cauldron in like 6 pulls.\' - Valores", -- 78
+"\'You have no job, disease sucker!.\' - Funkzorz", -- 79
+"\'One time I was in a pug that really pissed me off so I just drank like 10 of their flasks from their cauldron.\' - Thiona", -- 80
+"\'Jake, I am SUPER motivated right now.\' - Tawa (drunk)", -- 81
+"\'Yes, I did disengage off the platform. No, I don't want to talk about. Yes, you can go fuck yourselves.\' - Karahd", -- 82
+"\'i got life grip back, prepare to die\' - Bakescakes", -- 83
+"\'i hate fun.\' - Dhaline", -- 84
+"\'perfect, i hate condoms\' - Thiona", -- 85
+"\'I've been real good at balls\' - Twitch", -- 86
+"\'I'm trusted with government secrets\'-Anton \'That's because you won't remember them\'' -Prixah", -- 87
+"\'There's no time to get your keys, the house is on fire!\' -Jake", -- 88
+"\'I have a few holes I could fill.\' - Antonidious", -- 89
+"\'SAM WAIT YOU WON'T BE HERE FOR MY SAC\' - Tawariell",  -- 90
+"\'I believe I could beat up Anton in real life\' - Thiona", -- 91
+"\'Being motivated is overrated.  Think about it, every single corpse on Mt. Everest was once very motivated!\' - Meeracat", -- 92
+"\'one day i would like to give such knoledge that it may be included\' - Kennurd", -- 93
+"\'Please don't add that as my motivation\' - Kennurd", -- 94
+"\'I got hit by surging darkness because I sneezed\' - Tawa (with sadness)", -- 95
+"\'i am open to the idea of more weiner mail jess\' - Akronos", -- 96
+"\'Lack of porcupenis\' - Meeracat", -- 97
+"\'what it do\' - Prixah", -- 98
+"\'...shave and a haircut\' - Meera \'TWO CENTS\' - Ama", -- 99
+"\'Rowtron, say something inspiring!\' - Ben \'Uh...\' - Row \'Good enough!\' - Ben", -- 100
+"\'It just sits there all carrot-ey and shit and it makes me mad\' - Ben", -- 101
+"\'So you need more warning to make up for your mistake.\' - Energy", -- 102
+"\'So you hit Wake of Ashes... then the thing with the spinny hammers\' - Ben", -- 103
+"\'I'm actually in idiot\' - Lanille", -- 104
+"\'Twitchys is a crutch\' - Prixah", -- 105
+"\'I never die on accident\' - Semirhag", -- 106
+"\'We'll call it the \'fuck truck revolution\'\' - Prixah" -- 107
+}
+
+local events = CreateFrame("Frame")
+events:RegisterEvent("CHAT_MSG_GUILD")
+events:RegisterEvent("CHAT_MSG_OFFICER")
+events:RegisterEvent("CHAT_MSG_WHISPER")
+events:RegisterEvent("ENCOUNTER_END")
+
+lastquote = time()
+
+function tablelength(T)
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
+end
+
+local num_quotes = tablelength(GET_MOTIVATED)
+
+local function handleEvent(self, event, message, sender)
+--	print('Saw a message')
+--	print(event)
+--	print(message)
+--	print(sender)
+	local msg, id = strsplit(' ',message)
+--	print(msg)
+--	print(id)
+--	print(msg == "!motivateme")
+	if msg == "!motivateme" then
+--		print('Sending motivation')
+		local idnum = tonumber(id)
+		if idnum == nil or idnum <= 0 or idnum > num_quotes then
+			idnum = math.random(num_quotes)
+			print(idnum)
+		end
+		if  time() - lastquote < 3 then
+			SendChatMessage("The guild has been motivated recently, here is your personally tailored motivation:", "WHISPER", nil, sender)
+			SendChatMessage(GET_MOTIVATED[idnum], "WHISPER", nil, sender)
+		elseif event == "CHAT_MSG_WHISPER" then
+--			print('to guild chat')
+			SendChatMessage(strconcat(sender," requested motivation:"), "GUILD")
+			SendChatMessage(GET_MOTIVATED[idnum], "GUILD")
+			lastquote = time()
+		elseif event == "ENCOUNTER_END" then
+			print(event)
+			local raidsize = GetNumGroupMembers()
+			local guildraidsize = 0
+			for i in 1,raidsize do
+				pname = GetRaidRosterInfo(i)
+				if UnitIsInMyGuild(pname) then
+					guildraidsize = guildraidsize + 1
+				end
+			end
+			if raidsize == guildraidsize then
+				SendChatMessage(GET_MOTIVATED[math.random(num_quotes)], "GUILD")
+				lastquote = time()
+			end
+		end
+	end
+end
+
+events:SetScript("OnEvent",handleEvent)
